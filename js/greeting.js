4001f54399a6321426faa1081e12d8f31c913df3
adunnikuye <68731153+adunnikuye@users.noreply.github.com>
2021-04-23 05:28:52 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,15 +3,15 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' t o m i';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const gree1 = 'go to sleep  ';
+const gree2 = 'good morning  ';
+const gree3 = 'good afternoon  ';
+const gree4 = 'good evening,  ';
+const gree5 = 'good evening,  ';
+const gree6 = 'good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
